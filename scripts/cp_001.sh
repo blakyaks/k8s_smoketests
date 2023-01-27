5fe70abe40f70b85b7593b41fe79f49d0b05cbfe
Stuart <stuart.anderson@blakyaks.com>
2023-01-27 11:09:07 +0000

    Fleshing out cp_001 smoketest

--- scripts/cp_001.sh
+++ scripts/cp_001.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 clear
+echo "------------------------------------------"
 echo "AKS cluster smoketest"
 echo "------------------------------------------"
 echo
@@ -11,258 +12,249 @@ echo "Infra services"
 echo "---------------------------------------------------------------"
 echo
 echo "------------------------------------------"
-echo "Flux"
+echo "Flux - Check Flux git sources"
 echo "------------------------------------------"
-echo
-echo "Check Flux git sources"
 echo
 kubectl get gitrepositories -A | sed 's/ \+ /\t/g' | q -H -t "select READY, count(*) as cnt from - group by READY" -O -b
 echo
 kubectl get gitrepositories -A | sed 's/ \+ /\t/g' | q -H -t "select NAMESPACE, NAME, READY, STATUS from -" -O -b
 echo
-echo "Check Flux helm repo sources"
+echo "------------------------------------------"
+echo "Flux - Check Flux helm repo sources"
+echo "------------------------------------------"
 echo
 kubectl get helmrepositories -A | sed 's/ \+ /\t/g' | q -H -t "select READY, count(*) as cnt from - group by READY" -O -b
 echo
 kubectl get helmrepositories -A | sed 's/ \+ /\t/g' | q -H -t "select NAMESPACE, NAME, READY, STATUS from -" -O -b
 echo
-echo "Check Flux helm releases"
+echo "------------------------------------------"
+echo "Flux - Check Flux helm releases"
+echo "------------------------------------------"
 echo
 kubectl get helmrelease -A | sed 's/ \+ /\t/g' | q -H -t "select STATUS as helmrelease_status, count(*) as cnt from - group by STATUS" -O -b
 echo
 kubectl get helmrelease -A | sed 's/ \+ /\t/g' | q -H -t "select * from -" -O -b
 echo
-echo
-echo "Check Flux helm charts"
+echo "------------------------------------------"
+echo "Flux - Check Flux helm charts"
+echo "------------------------------------------"
 echo
 kubectl get helmcharts -A | sed 's/ \+ /\t/g' | q -H -t "select READY, count(*) as cnt from - group by READY" -O -b
 echo
 kubectl get helmcharts -A | sed 's/ \+ /\t/g' | q -H -t "select NAMESPACE, NAME, READY, STATUS from -" -O -b
 echo
-echo "Check Flux Kustomizations"
+echo "------------------------------------------"
+echo "Flux - Check Flux Kustomizations"
+echo "------------------------------------------"
 echo
 kubectl get kustomizations -A | sed 's/ \+ /\t/g' | q -H -t "select READY, count(*) as cnt from - group by READY" -O -b
 echo
 kubectl get kustomizations -A | sed 's/ \+ /\t/g' | q -H -t "select * from -" -O -b
 echo
 echo "------------------------------------------"
-echo "Azure AD pod identity"
+echo "AAD podId - Check MIC reconciling"
 echo "------------------------------------------"
-echo
-echo "Check MIC appears to be reconciling ok"
 echo
 kubectl logs --selector "app.kubernetes.io/component"="mic" -n pod-identity-system --all-containers=true --prefix=true --timestamps=true | grep "reconciling identity assignment"
 echo
-echo "Check MIC for errors or warnings"
+echo "------------------------------------------"
+echo "AAD podId - Errors or warnings"
+echo "------------------------------------------"
 echo
 kubectl logs --selector "app.kubernetes.io/component"="mic" -n pod-identity-system --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
-echo "Check the managed identity controller (MIC) logs"
-echo
-kubectl logs --selector "app.kubernetes.io/component"="mic" -n pod-identity-system --all-containers=true --prefix=true --timestamps=true
-echo
 echo "------------------------------------------"
-echo "Cert-manager"
+echo "Cert-manager - Errors or warnings"
 echo "------------------------------------------"
-echo
-echo "Check cert-manager logs for errors or warnings"
 echo
 kubectl logs --selector "app"="cert-manager" -n ingress-system --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
-echo "Check cert-manager logs"
-echo
-kubectl logs --selector "app"="cert-manager" -n ingress-system --all-containers=true --prefix=true --timestamps=true
-echo
 echo "------------------------------------------"
-echo "External-dns"
+echo "External-dns - **zone not found** errors"
 echo "------------------------------------------"
-echo
-echo "Check external-dns logs for **zone not found** errors"
 echo
 kubectl logs --selector "app.kubernetes.io/instance"="external-dns" -n ingress-system --all-containers=true --prefix=true --timestamps=true | grep "Azure DNS zone was not found" -i
 echo
-echo "Check external-dns logs for **No endpoints could be generated** errors"
+echo "------------------------------------------"
+echo "External-dns - **No endpoints could be generated** errors"
+echo "------------------------------------------"
 echo
 kubectl logs --selector "app.kubernetes.io/instance"="external-dns" -n ingress-system --all-containers=true --prefix=true --timestamps=true | grep "No endpoints could be generated" -i
 echo
-echo "Check external-dns logs for errors or warnings"
+echo "------------------------------------------"
+echo "External-dns - Errors or warnings"
+echo "------------------------------------------"
 echo
 kubectl logs --selector "app.kubernetes.io/instance"="external-dns" -n ingress-system --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
-echo "Check external-dns logs"
-echo
-kubectl logs --selector "app.kubernetes.io/instance"="external-dns" -n ingress-system --all-containers=true --prefix=true --timestamps=true
-echo
 echo "------------------------------------------"
-echo "Kyverno"
+echo "Kyverno - Storage location is valid"
 echo "------------------------------------------"
-echo
-echo "Check Kyverno backup storage location is valid"
 echo
 kubectl logs --selector "app.kubernetes.io/instance"="velero" -n velero --all-containers=true --prefix=true --timestamps=true | grep "BackupStorageLocations is valid" -i
 echo
-echo "Check Kyverno logs for errors or warnings"
+echo "------------------------------------------"
+echo "Kyverno - Errors or warnings"
+echo "------------------------------------------"
 echo
 kubectl logs --selector "app.kubernetes.io/instance"="kyverno" -n kyverno-system --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
-echo
-echo "Check Kyverno logs"
-echo
-kubectl logs --selector "app.kubernetes.io/instance"="kyverno" -n kyverno-system --all-containers=true --prefix=true --timestamps=true
 echo
 echo "---------------------------------------------------------------"
 echo "Application services"
 echo "---------------------------------------------------------------"
 echo
 echo "------------------------------------------"
-echo "Orchestrate API"
+echo "Orchestrate API - *network exists* error"
 echo "------------------------------------------"
 echo
-echo "Check for Orchestrate *network exists* error"
-echo
 kubectl logs deployment/orchestrate-api -n orchestrate --all-containers=true --prefix=true --timestamps=true | grep "a chain with the same name already exists" -i
+echo
+echo "------------------------------------------"
+echo "Orchestrate API - Errors and warnings"
+echo "------------------------------------------"
 echo
 kubectl logs deployment/orchestrate-api -n orchestrate --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Orchestrate tx-listener"
+echo "Orchestrate tx-listener - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/orchestrate-tx-listener -n orchestrate --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Orchestrate tx-sender"
+echo "Orchestrate tx-sender - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/orchestrate-tx-sender -n orchestrate --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Orchestrate quorum-key-manager"
+echo "Orchestrate quorum-key-manager - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/quorum-key-manager -n orchestrate --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Orchestrate vault-configurer"
+echo "Orchestrate vault-configurer - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/vault-configurer -n orchestrate --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Besu explorer"
+echo "Besu explorer - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/explorer -n besu --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Besu explorer-api"
+echo "Besu explorer-api - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/explorer-api -n besu --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Besu explorer-ingestion"
+echo "Besu explorer-ingestion - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/explorer-ingestion -n besu --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Besu explorer-mongodb"
+echo "Besu explorer-mongodb - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/explorer-mongodb -n besu --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Carbon-place carbon-external"
+echo "Carbon-place carbon-external - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/carbon-external -n carbon-place --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Carbon-place carbon-front"
+echo "Carbon-place carbon-front - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/carbon-front -n carbon-place --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Codefi-assets admin-api"
+echo "Codefi-assets admin-api - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/admin-api -n codefi-assets --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Codefi-assets assets-api"
+echo "Codefi-assets assets-api - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/assets-api -n codefi-assets --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Codefi-assets assets-front"
+echo "Codefi-assets assets-front - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/assets-front -n codefi-assets --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Codefi-assets entity-api"
+echo "Codefi-assets entity-api - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/entity-api -n codefi-assets --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Codefi-assets external-identity-api"
+echo "Codefi-assets external-identity-api - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/external-identity-api -n codefi-assets --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Codefi-assets i18n-api"
+echo "Codefi-assets i18n-api - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/i18n-api -n codefi-assets --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Codefi-assets kyc-api"
+echo "Codefi-assets kyc-api - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/kyc-api -n codefi-assets --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Codefi-assets mailing-api"
+echo "Codefi-assets mailing-api - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/mailing-api -n codefi-assets --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Codefi-assets metadata-api"
+echo "Codefi-assets metadata-api - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/metadata-api -n codefi-assets --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Codefi-assets smart-contract-api"
+echo "Codefi-assets smart-contract-api - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/smart-contract-api -n codefi-assets --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Codefi-assets workflow-api"
+echo "Codefi-assets workflow-api - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/workflow-api -n codefi-assets --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Kafka confluent-stack-cp-schema-registry"
+echo "Kafka confluent-stack-cp-schema-registry - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/confluent-stack-cp-schema-registry -n kafka --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "Kafka kafka-ui"
+echo "Kafka kafka-ui - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/kafka-ui -n kafka --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
 echo
 echo "------------------------------------------"
-echo "pgadmin"
+echo "pgadmin - Errors and warnings"
 echo "------------------------------------------"
 echo
 kubectl logs deployment/pgadmin -n pgadmin --all-containers=true --prefix=true --timestamps=true | grep "error\|warning" -i
@@ -271,14 +263,18 @@ echo "---------------------------------------------------------------"
 echo "All resources"
 echo "---------------------------------------------------------------"
 echo
+echo "------------------------------------------"
 echo "Check all pods statuses"
+echo "------------------------------------------"
 echo
 kubectl get pods -A | sed 's/ \+ /\t/g' | q -H -t "select STATUS, count(*) as cnt from - group by STATUS" -O -b
 echo
+echo "------------------------------------------"
 echo "Check all pods restarts"
+echo "------------------------------------------"
 echo
 kubectl get pods -A | sed 's/ \+ /\t/g' | q -H -t "select * from - where RESTARTS != '0'" -O -b
 echo
-echo "Check policy violations"
-echo
-kubectl get events | sed 's/ \+ /\t/g' | q -H -t "select OBJECT as policy, count(*) as cnt from - where REASON ='PolicyViolation' group by OBJECT order by cnt DESC" -O -b
+echo "------------------------------------------"
+echo "Report ends"
+echo "------------------------------------------"
